33d04982609aaa6be468a7d8bb2807a558cf4836
Jake Wilson <hi@jkwlsn.dev>
2025-07-17 21:20:49 +0100

    Adds `SiteMain` component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import SiteHeader from './components/SiteHeader.vue';
+import SiteMain from './components/SiteMain.vue';
 </script>
 
 <template>
   <SiteHeader />
+  <SiteMain />
 </template>
