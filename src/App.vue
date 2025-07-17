54b7fe75eba7afd60849bb31222c22771c6bea5e
Jake Wilson <hi@jkwlsn.dev>
2025-07-17 21:21:29 +0100

    Adds `SiteFooter` component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import SiteHeader from './components/SiteHeader.vue';
 import SiteMain from './components/SiteMain.vue';
+import SiteFooter from './components/SiteFooter.vue';
 </script>
 
 <template>
   <SiteHeader />
   <SiteMain />
+  <SiteFooter />
 </template>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,9 @@
+<script setup>
+const copyright = 'Copyright Jake, 2025';
+</script>
+
+<template>
+  <footer>
+    <small>{{ copyright }}</small>
+  </footer>
+</template>
