77d4741652ba20ffc35583d70c8f825ba64d9029
Jake Wilson <hi@jkwlsn.dev>
2025-07-31 16:16:57 +0100

    fixup SiteMain

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,22 +1,15 @@
 <script setup>
-import { useUserSettings } from '../composables/useUserSettings';
 import PollenSensitivity from './PollenSensitivity.vue';
 import GeoLocation from './GeoLocation.vue';
 import PollenData from './PollenData.vue';
-import PollenAlert from './PollenAlert.vue';
-
-const { settings } = useUserSettings();
+import PollenAlerts from './PollenAlerts.vue';
 </script>
 
 <template>
   <main>
-    <PollenSensitivity v-model="settings.sensitivity" />
-    <GeoLocation v-model:location="settings.location" />
-    <PollenAlert
-      :pollenData="pollenData"
-      :times="times"
-      :threshold="settings.sensitivity"
-    />
+    <PollenSensitivity />
+    <GeoLocation />
+    <PollenAlerts />
     <PollenData />
   </main>
 </template>
