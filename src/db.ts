27b10c7a27725ee61f2c47796219f1d57e59512c
Jake Wilson <hi@jkwlsn.dev>
2025-10-01 16:46:26 +0100

    Creates Symptom type and DB schema

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,16 @@
 import Dexie, { type EntityTable } from "dexie";
 import { DATABASE_NAME, DATABASE_VERSION } from "./config";
+import { type Symptom } from "./interfaces/symptom";
 
-interface Test {
-  id: number;
-}
-
+// Instantiate db
 const db = new Dexie(DATABASE_NAME) as Dexie & {
-  symptoms: EntityTable<Test, "id">;
+  symptoms: EntityTable<Symptom, "id">;
 };
 
 // Declare Schema
 db.version(DATABASE_VERSION).stores({
-  symptoms: "++id",
+  symptoms: "++id, timestamp, type",
 });
 
 // Exports
-export type { Test };
 export { db };
