fe9d5466617690f07e96baf7946f9ce53d3cc018
Jake Wilson <hi@jkwlsn.dev>
2025-10-01 16:44:36 +0100

    Centralise symptoms in app config
    Adds `SYMPTOMS_LIST` to `config.ts` to centralise the information. The list is a simple array, which can be updated in the future

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,11 @@
 export const DATABASE_NAME: string = "snotDB";
 export const DATABASE_VERSION: number = 1;
+export const SYMPTOM_LIST: string[] = [
+  "Sneezing",
+  "Snotty nose",
+  "Sore throat",
+  "Coughing",
+  "Itchy eyes",
+  "Headaches",
+  "Tiredness",
+];
