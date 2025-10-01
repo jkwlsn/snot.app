a75d868dd2ec6ad5464d8162bc50e0c778e4fd24
Jake Wilson <hi@jkwlsn.dev>
2025-10-01 16:47:04 +0100

    Adds input form for logging symptoms
    The checklist form allows multiple symptoms to be selected at once and
    all added to the database. Each symptom is given its own record in the
    DB. When the form is successfully submitted, the from is reset.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import SymptomInput from "./components/SymptomInput.vue";
 </script>
 
 <template>
+  <SymptomInput />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
