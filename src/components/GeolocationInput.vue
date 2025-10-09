ab2b97e894299e0f669a4e68fe7ab0c2667b5f9c
Jake Wilson <hi@jkwlsn.dev>
2025-10-09 18:48:35 +0100

    Refactor Geolocation to its own singleton composable
    Moving the Geolocation code outside of the component is necessary for
    creating a singleton, which will allow other components to access the
    geolocation data, e.g. for recording symptom locations.

--- src/components/GeolocationInput.vue
+++ src/components/GeolocationInput.vue
@@ -6,14 +6,14 @@
         placeholder="e.g., Paris, France..."
         v-model="textLocation"
       />
-      <button :disabled="isLoading" @click="submitTextLocation">
+      <button :disabled="anyLoading" @click="submitTextLocation">
         Find my location
       </button>
     </fieldset>
     <fieldset>
       <button
         id="requestGeolocation"
-        :disabled="isLoading"
+        :disabled="anyLoading"
         @click="requestGeolocation"
       >
         {{ gpsButtonText }}
@@ -29,86 +29,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import { useNominatim } from "../composables/useNominatim";
-import type { Coordinates } from "../types/coordinates";
-
-const nominatim = useNominatim();
-
-const gpsButtonText = ref<"Use GPS" | "Refresh GPS">("Use GPS");
-const textLocation = ref<string>("");
-const location = ref<Coordinates | null>(null);
-const errorMessage = ref<string | null>(null);
-const isLoading = ref<boolean>(false);
-
-const anyLoading = computed(
-  () => isLoading.value || nominatim.nominatimIsLoading.value,
-);
-
-const anyError = computed(
-  () => errorMessage.value || nominatim.nominatimErrorMessage.value || null,
-);
-
-const submitTextLocation = async (): Promise<void> => {
-  location.value = null;
-  errorMessage.value = null;
-
-  const newLocation = await nominatim.forwardGeocode(textLocation.value);
-
-  if (newLocation) {
-    location.value = newLocation;
-  }
-  gpsButtonText.value = "Use GPS";
-};
-
-const requestGeolocation = (): void => {
-  isLoading.value = true;
-  errorMessage.value = null;
-  location.value = null;
-
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else {
-    errorMessage.value = "Geolocation is not available";
-    isLoading.value = false;
-  }
-};
-
-const success = (position: GeolocationPosition): void => {
-  const newLocation: Coordinates = {
-    latitude: position.coords.latitude,
-    longitude: position.coords.longitude,
-  };
-  location.value = newLocation;
-  reverseGeocode(newLocation);
-
-  isLoading.value = false;
-  gpsButtonText.value = "Refresh GPS";
-};
-
-const reverseGeocode = async (coordinates: Coordinates) => {
-  const resultText = await nominatim.reverseGeocode(coordinates);
-  if (resultText) {
-    textLocation.value = resultText;
-  } else {
-    textLocation.value = "";
-  }
-};
-
-const error = (err: GeolocationPositionError): void => {
-  isLoading.value = false;
-  switch (err.code) {
-    case err.PERMISSION_DENIED:
-      errorMessage.value = "User denied the request for Geolocation.";
-      break;
-    case err.POSITION_UNAVAILABLE:
-      errorMessage.value = "Location information is unavailable.";
-      break;
-    case err.TIMEOUT:
-      errorMessage.value = "The request to get user location timed out.";
-      break;
-    default:
-      errorMessage.value = "An unknown error occurred.";
-  }
-};
+import { useGeolocation } from "../composables/useGeolocation";
+const {
+  gpsButtonText,
+  textLocation,
+  location,
+  anyLoading,
+  anyError,
+  submitTextLocation,
+  requestGeolocation,
+} = useGeolocation();
 </script>
