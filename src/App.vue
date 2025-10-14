1696e9682e4c35b283236bc9259cb32b020e9752
Jake Wilson <hi@jkwlsn.dev>
2025-10-14 11:34:20 +0100

    Add pollen data filter component
    This component is more a proof of concept, it allows for debugging of
    the pollen data filter utility. It could be used as an input to a graph.

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import SymptomInput from "./components/SymptomInput.vue";
 import SymptomLog from "./components/SymptomLog.vue";
 import GeolocationInput from "./components/GeolocationInput.vue";
 import PollenData from "./components/PollenData.vue";
+import PollenDataPicker from "./components/PollenDataPicker.vue";
 </script>
 
 <template>
   <GeolocationInput />
   <SymptomInput />
   <SymptomLog />
+  <PollenDataPicker />
   <PollenData />
 </template>
 
