505264b053f8dd7621c5611eaa038641dca64a15
Jake Wilson <hi@jkwlsn.dev>
2025-10-14 11:34:20 +0100

    Add pollen data filter component
    This component is more a proof of concept, it allows for debugging of
    the pollen data filter utility. It could be used as an input to a graph.

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import SymptomInput from "./components/SymptomInput.vue";
 import SymptomLog from "./components/SymptomLog.vue";
 import GeolocationInput from "./components/GeolocationInput.vue";
 import PollenData from "./components/PollenData.vue";
+import PollenDataFilter from "./components/PollenDataFilter.vue";
 </script>
 
 <template>
   <GeolocationInput />
   <SymptomInput />
   <SymptomLog />
+  <PollenDataFilter />
   <PollenData />
 </template>
 
---
+++ src/components/PollenDataFilter.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <h2>Filter Pollen Data</h2>
+    <form>
+      <fieldset>
+        <div>
+          <label for="startTimeInput">Start</label>
+          <input
+            type="datetime-local"
+            id="startTimeInput"
+            v-model="startTime"
+            :min="minimumDateTime"
+            step="3600"
+          />
+        </div>
+        <div>
+          <label for="endTimeInput">End</label>
+          <input
+            type="datetime-local"
+            id="endTimeInput"
+            v-model="endTime"
+            :min="startTime"
+            step="3600"
+          />
+        </div>
+      </fieldset>
+    </form>
+    <pre>{{ JSON.stringify(filteredLevels, null, 2) }}</pre>
+  </section>
+</template>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useOpenMeteoAPI } from "../composables/useOpenMeteo";
+import {
+  RawPollenData,
+  usefilterPollenDataByTimeframe,
+} from "../utils/filterPollenLevelsByTimeframe.ts";
+
+const { data } = useOpenMeteoAPI();
+const filter = usefilterPollenDataByTimeframe();
+
+const minimumDateTime = new Date().toISOString().split(":")[0] + ":00";
+const startTime = ref<string>(minimumDateTime);
+const endTime = ref<string>("");
+
+const filteredLevels = computed(() => {
+  if (!data.value?.hourly) {
+    return [];
+  }
+
+  return filter.filterPollenDataByTimeframe(
+    data.value.hourly as unknown as RawPollenData,
+    startTime.value,
+    endTime.value,
+  );
+});
+</script>
