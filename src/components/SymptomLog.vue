b9df64668903158b20f37f2dcc60116f9258df84
Jake Wilson <hi@jkwlsn.dev>
2025-10-23 12:56:14 +0100

    Splits symptom data into composable for re-use
    The Symptom data will need to be used by the new graphs. This
    refactoring splits the logic out of the component into a composable

--- src/components/SymptomLog.vue
+++ src/components/SymptomLog.vue
@@ -3,30 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from "vue";
-import { db } from "../db";
-import { liveQuery } from "dexie";
-import { SymptomRecord } from "../interfaces/SymptomRecord";
+import { useSymptoms } from "../composables/useSymptoms";
 
-const symptoms = ref<SymptomRecord[]>([]);
-
-let liveSubscription: { unsubscribe: () => void } | null = null;
-
-const subscribeToLiveQuery = () => {
-  liveSubscription = liveQuery(() => db.symptoms.toArray()).subscribe(
-    (updatedSymptoms: SymptomRecord[]) => {
-      symptoms.value = updatedSymptoms;
-    },
-  );
-};
-
-onMounted(() => {
-  subscribeToLiveQuery();
-});
-
-onBeforeUnmount(() => {
-  if (liveSubscription) {
-    liveSubscription.unsubscribe();
-  }
-});
+const { symptoms } = useSymptoms();
 </script>
---
+++ src/composables/useSymptoms.ts
@@ -0,0 +1,30 @@
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { db } from "../db";
+import { liveQuery } from "dexie";
+import { SymptomRecord } from "../interfaces/SymptomRecord";
+
+export function useSymptoms() {
+  const symptoms = ref<SymptomRecord[]>([]);
+
+  let liveSubscription: { unsubscribe: () => void } | null = null;
+
+  const subscribeToLiveQuery = () => {
+    liveSubscription = liveQuery(() => db.symptoms.toArray()).subscribe(
+      (updatedSymptoms: SymptomRecord[]) => {
+        symptoms.value = updatedSymptoms;
+      },
+    );
+  };
+
+  onMounted(() => {
+    subscribeToLiveQuery();
+  });
+
+  onBeforeUnmount(() => {
+    if (liveSubscription) {
+      liveSubscription.unsubscribe();
+    }
+  });
+
+  return { symptoms };
+}
