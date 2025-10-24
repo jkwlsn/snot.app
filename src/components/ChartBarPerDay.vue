adb5e44632c369987a1d1a1fdffbeca288a8b156
Jake Wilson <hi@jkwlsn.dev>
2025-10-24 13:57:41 +0100

    Add generic Bar Chart component
    This component create a chartjs bar chart. It accepts options, data, and
    an ID and title. This generic component can be used for any bar charts.
    
    The useDailyBarChart composable converts the sorted, aggregated symptom
    data into the data structure required by `chart.js` bar charts.

---
+++ src/components/ChartBarPerDay.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>{{ props.chartTitle }}</h3>
+  <Bar :id="props.id" :options="props.options" :data="props.data" />
+</template>
+
+<script setup lang="ts">
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  LinearScale,
+  TimeScale,
+} from "chart.js";
+import "chartjs-adapter-date-fns";
+import type { ChartData, ChartOptions } from "chart.js";
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, LinearScale, TimeScale);
+
+interface Props {
+  id: string;
+  chartTitle: string;
+  data: ChartData<"bar", { x: number; y: number }[]>;
+  options: ChartOptions<"bar">;
+}
+const props = defineProps<Props>();
+</script>
