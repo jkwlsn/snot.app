ebfa19119820ac2fd591819a0ad226758eb24624
Jake Wilson <hi@jkwlsn.dev>
2025-10-28 12:36:16 +0000

    Add basic line chart vue-chartjs component
    This component wraps vue-chartjs Line chart. It provides basic
    templating and props and an interface for those props.

---
+++ src/components/ChartLine.vue
@@ -0,0 +1,40 @@
+<template>
+  <h3>{{ props.chartTitle }}</h3>
+  <Line :id="props.id" :options="props.options" :data="props.data" />
+</template>
+
+<script setup lang="ts">
+import { Line } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+} from "chart.js";
+import "chartjs-adapter-date-fns";
+import type { ChartData, ChartOptions } from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+);
+
+interface Props {
+  id: string;
+  chartTitle: string;
+  data: ChartData<"line">;
+  options: ChartOptions<"line">;
+}
+const props = defineProps<Props>();
+</script>
