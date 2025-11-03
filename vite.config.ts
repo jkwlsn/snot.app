69526ef0f4d94f0974e1734ef6ee1f4686a3d913
Jake Wilson <hi@jkwlsn.dev>
2025-11-03 13:42:33 +0000

    Installs Tailwind CSS and adds to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -95,5 +96,6 @@ export default defineConfig({
         ],
       },
     }),
+    tailwindcss(),
   ],
 });
