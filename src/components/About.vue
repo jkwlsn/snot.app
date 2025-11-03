09d927d0b8cf0658ef7caeeb0f8480465232179d
Jake Wilson <hi@jkwlsn.dev>
2025-11-03 15:24:51 +0000

    Add basic title styles
    - Centred
    - Bolded
    - margin-bottom
    - Title case (except h1)

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,21 +1,19 @@
 <template>
-  <section>
-    <h1>snot.app</h1>
-    <p>
-      <strong>snot.app</strong> is an offline-first web app designed to track
-      and predict hayfever symptoms.
-    </p>
-    <p>
-      It's also a toy project by
-      <a rel="external" href="https://github.com/jkwlsn">@jkwlsn</a> designed to
-      test out TypeScript, Vue 3+, Web APIs, Tailwind CSS, and a variety of
-      local development tools including Vite, Eslint, and Prettier.
-    </p>
-    <h2>How to use</h2>
-    <ol>
-      <li>🌍 📌 Set your location to see local pollen levels</li>
-      <li>🌼 🤧 Log your hayfever symptoms</li>
-      <li>😀 👍 Enjoy!</li>
-    </ol>
-  </section>
+  <h1 class="text-center font-bold mb-3 lowercase">snot.app</h1>
+  <p>
+    <strong>snot.app</strong> is an offline-first web app designed to track and
+    predict hayfever symptoms.
+  </p>
+  <p>
+    It's also a toy project by
+    <a rel="external" href="https://github.com/jkwlsn">@jkwlsn</a> designed to
+    test out TypeScript, Vue 3+, Web APIs, Tailwind CSS, and a variety of local
+    development tools including Vite, Eslint, and Prettier.
+  </p>
+  <h2>How to use</h2>
+  <ol>
+    <li>🌍 📌 Set your location to see local pollen levels</li>
+    <li>🌼 🤧 Log your hayfever symptoms</li>
+    <li>😀 👍 Enjoy!</li>
+  </ol>
 </template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,7 @@
     class="p-3 mx-3 my-6 bg-green-100 rounded-lg shadow-lg md:max-w-3/4 md:mx-auto lg:max-w-1/2"
   >
     <header>
-      <h2>
+      <h2 class="text-center font-bold my-3 capitalize">
         <slot name="title" />
       </h2>
     </header>
