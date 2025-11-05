746e73a3a85b1da6b4790f822cba5c4789e32495
Jake Wilson <hi@jkwlsn.dev>
2025-11-05 11:11:24 +0000

    Refactor pollen and symptom tables into `DataTable`
    The two data tables share a lot in common, this commit refactors those
    shared properties into a single `DataTable` component which accepts a
    list of records and a list of headers. The individual pollen and Symptom
    components now prepare their data to match this interface.

--- src/components/PollenDataTable.vue
+++ src/components/PollenDataTable.vue
@@ -1,51 +1,35 @@
 <template>
-  <div class="py-3 overflow-x-auto">
-    <table
-      v-if="records.length"
-      class="table-auto min-w-full divide-y divide-green-100"
-    >
-      <thead class="bg-gray-100">
-        <tr>
-          <th class="p-3 text-start whitespace-nowrap text-xs capitalize">
-            Time
-          </th>
-          <th
-            v-for="pollenType in pollenTypes"
-            :key="pollenType"
-            class="p-3 text-start text-xs capitalize whitespace-nowrap"
-          >
-            {{ pollenType.replace(/_/g, " ").replace(/pollen/g, "pollen") }}
-          </th>
-        </tr>
-      </thead>
-      <tbody class="bg-white divide-y divide-green-100">
-        <tr
-          v-for="record in records"
-          :key="record.timestamp.toISOString()"
-          class="hover:bg-gray-100"
-        >
-          <td class="p-3 whitespace-nowrap text-sm text-end">
-            {{ new Date(record.timestamp).toLocaleString() }}
-          </td>
-          <td
-            v-for="pollenType in pollenTypes"
-            :key="pollenType"
-            class="p-3 whitespace-nowrap text-sm text-left"
-          >
-            {{ record.levels[pollenType] }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div v-else class="text-center text-gray-500 mt-2">No data</div>
-  </div>
+  <DataTable :headers="headers" :records="transformedRecords" />
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import type { PollenRecord } from "../interfaces/Pollen";
+import DataTable from "./DataTable.vue";
 
-defineProps<{
+const props = defineProps<{
   records: PollenRecord[];
   pollenTypes: string[];
 }>();
+
+const headers = computed(() => [
+  "Time",
+  ...props.pollenTypes.map((pollenType) =>
+    pollenType.replace(/_/g, " ").replace(/pollen/g, "pollen")
+  ),
+]);
+
+const transformedRecords = computed(() =>
+  props.records.map((record) => {
+    const transformedRecord: Record<string, any> = {
+      Time: new Date(record.timestamp).toLocaleString(),
+    };
+    for (const pollenType of props.pollenTypes) {
+      transformedRecord[
+        pollenType.replace(/_/g, " ").replace(/pollen/g, "pollen")
+      ] = record.levels[pollenType];
+    }
+    return transformedRecord;
+  })
+);
 </script>
--- src/components/SymptomDataTable.vue
+++ src/components/SymptomDataTable.vue
@@ -1,48 +1,23 @@
 <template>
-  <div class="py-3 overflow-x-auto">
-    <table
-      v-if="records.length"
-      class="table-auto min-w-full divide-y divide-green-100"
-    >
-      <thead class="bg-gray-100">
-        <tr>
-          <th class="p-3 text-start whitespace-nowrap text-xs capitalize">
-            Time
-          </th>
-          <th class="p-3 text-start whitespace-nowrap text-xs capitalize">
-            Symptom
-          </th>
-          <th class="p-3 text-start whitespace-nowrap text-xs capitalize">
-            Severity
-          </th>
-        </tr>
-      </thead>
-      <tbody class="bg-white divide-y divide-green-100">
-        <tr
-          v-for="record in records"
-          :key="record.id"
-          class="hover:bg-gray-100"
-        >
-          <td class="p-3 text-start whitespace-nowrap text-sm">
-            {{ new Date(record.timestamp).toLocaleString() }}
-          </td>
-          <td class="p-3 text-start whitespace-nowrap text-sm">
-            {{ record.type }}
-          </td>
-          <td class="p-3 text-start whitespace-nowrap text-sm">
-            {{ record.severity }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div v-else class="text-center text-gray-500 mt-2">No data</div>
-  </div>
+  <DataTable :headers="headers" :records="transformedRecords" />
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import type { SymptomRecord } from "../interfaces/SymptomRecord";
+import DataTable from "./DataTable.vue";
 
-defineProps<{
+const props = defineProps<{
   records: SymptomRecord[];
 }>();
+
+const headers = ["Time", "Symptom", "Severity"];
+
+const transformedRecords = computed(() =>
+  props.records.map((record) => ({
+    Time: new Date(record.timestamp).toLocaleString(),
+    Symptom: record.type,
+    Severity: record.severity,
+  }))
+);
 </script>
