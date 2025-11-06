51240e45828941eb3d376779ae044b71e19333ba
Jake Wilson <hi@jkwlsn.dev>
2025-11-06 16:42:42 +0000

    Update `SymptomDataTable` to use new locale utility function
    Display the timestamp according to the locale using the new
    `formatDateToLocaleString` utility function, rather than the native JS
    functions.

--- src/components/SymptomDataTable.vue
+++ src/components/SymptomDataTable.vue
@@ -6,6 +6,7 @@
 import { computed } from "vue";
 import type { SymptomRecord } from "../interfaces/SymptomRecord";
 import DataTable from "./DataTable.vue";
+import { formatDateToLocaleString } from "../utils/dateUtils";
 
 const props = defineProps<{
   records: SymptomRecord[];
@@ -15,7 +16,7 @@ const headers = ["Time", "Symptom", "Severity"];
 
 const transformedRecords = computed(() =>
   props.records.map((record) => ({
-    Time: new Date(record.timestamp).toLocaleString(),
+    Time: formatDateToLocaleString(record.timestamp, { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: '2-digit' }),
     Symptom: record.type,
     Severity: record.severity,
   }))
