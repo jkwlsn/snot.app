8f4d7359c9554ca39917ddfa877d75f937744c3c
Jake Wilson <hi@jkwlsn.dev>
2025-11-14 14:11:32 +0000

    Remove `.orderBy()` from `liveQuery()`
    For some reason, orderBy() was interferring with Vue's reactivity. By
    removing this, I ensure that the data arrives in the expected order and
    reactivity is enabled.

--- src/composables/useSymptoms.ts
+++ src/composables/useSymptoms.ts
@@ -1,11 +1,11 @@
-import { type Ref, ref } from "vue";
-import { db } from "../db";
+import { ref, type Ref } from "vue";
 import { liveQuery } from "dexie";
+import { db } from "../db";
 import { type SymptomRecord } from "../interfaces/SymptomRecord";
 
 const symptoms = ref<SymptomRecord[]>([]);
 
-liveQuery(() => db.symptoms.orderBy("timestamp").toArray()).subscribe(
+liveQuery(() => db.symptoms.toArray()).subscribe(
   (updatedSymptoms: SymptomRecord[]) => {
     symptoms.value = updatedSymptoms;
   },
