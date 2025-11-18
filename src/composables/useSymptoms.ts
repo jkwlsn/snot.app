c72fcd362763d479f77cc6b25e575d6948de3d11
Jake Wilson <hi@jkwlsn.dev>
2025-11-18 13:31:11 +0000

    Add `delete` function to `useSymptoms`
    This function deletes symptoms from the database according to their ID.
    
    It's exported from useSymptoms to be injected elsewhere.

--- src/composables/useSymptoms.ts
+++ src/composables/useSymptoms.ts
@@ -5,12 +5,20 @@ import { type SymptomRecord } from "../interfaces/SymptomRecord";
 
 const symptoms = ref<SymptomRecord[]>([]);
 
+async function deleteSymptom(id: number): Promise<number> {
+  await db.symptoms.delete(id);
+  return id;
+}
+
 liveQuery(() => db.symptoms.toArray()).subscribe(
   (updatedSymptoms: SymptomRecord[]) => {
     symptoms.value = updatedSymptoms.reverse();
   },
 );
 
-export function useSymptoms(): { symptoms: Ref<SymptomRecord[]> } {
-  return { symptoms };
+export function useSymptoms(): {
+  symptoms: Ref<SymptomRecord[]>;
+  deleteSymptom: (id: number) => Promise<number>;
+} {
+  return { deleteSymptom, symptoms };
 }
