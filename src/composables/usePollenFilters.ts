9ca85092520beda5d6b765d73e310feaa570d4ca
Jake Wilson <hi@jkwlsn.dev>
2025-11-19 12:26:56 +0000

    Refactor `applyLevelFilter` out of filter functions in `usePollenFilters
    Both filter functions in `usePollenFilters` had duplicated code. This
    code is now split into its own helper function, `applyLevelFilter`,
    simplifying the filter logic.

--- src/composables/usePollenFilters.ts
+++ src/composables/usePollenFilters.ts
@@ -1,37 +1,38 @@
 import { type PollenData, type PollenRecord } from "../interfaces/Pollen";
 import { type PollenLevels, type PollenType } from "../interfaces/PollenTypes";
 
-// This function takes a PollenData object, defined in /interfaces/pollen,
-// and a number representing the pollen level threshold for the filter.
-// It returns a new PollenData object containing only the pollen types with
-// levels above the threshold.
+function applyLevelFilter(
+  record: PollenRecord,
+  levelFilterFn: (pollenType: PollenType, level: number | null) => boolean,
+): PollenRecord | null {
+  const newLevels: PollenLevels = { ...record.levels };
+  let hasValidPollenLevel = false;
+
+  for (const pollenType in newLevels) {
+    const level = newLevels[pollenType];
+    if (!levelFilterFn(pollenType, level)) {
+      newLevels[pollenType] = null;
+    }
+    if (newLevels[pollenType] !== null) {
+      hasValidPollenLevel = true;
+    }
+  }
+
+  return hasValidPollenLevel ? { ...record, levels: newLevels } : null;
+}
+
 export function filterPollenByLevel(
   pollenData: PollenData,
   minLevel: number,
 ): PollenData {
   const newRecords = pollenData.records
-    .map((record) => {
-      // map over all recorsd in pollenData
-      const newLevels: PollenLevels = { ...record.levels };
-      let hasHighPollen = false;
-
-      // Create new array containing pollens with levels above threshold
-      for (const pollenType in newLevels) {
-        const level = newLevels[pollenType];
-        // If the pollen level is below the threshold, set it to null
-        if (level !== null && level < minLevel) {
-          newLevels[pollenType] = null;
-        }
-        // Otherwise, set hasHighPollen to true
-        if (newLevels[pollenType] !== null) {
-          hasHighPollen = true;
-        }
-      }
-
-      return hasHighPollen ? { ...record, levels: newLevels } : null;
-    })
+    .map((record) =>
+      applyLevelFilter(
+        record,
+        (_, level) => level !== null && level >= minLevel,
+      ),
+    )
     .filter((record): record is PollenRecord => record !== null);
-  //filter out all records which contain null pollen level
 
   return {
     ...pollenData,
@@ -44,21 +45,11 @@ export function filterPollenByType(
   selectedPollenTypes: PollenType[],
 ): PollenData {
   const newRecords = pollenData.records
-    .map((record) => {
-      const newLevels: PollenLevels = { ...record.levels };
-      let isSelectedPollenType = false;
-
-      for (const pollenType in newLevels) {
-        if (!selectedPollenTypes.includes(pollenType)) {
-          newLevels[pollenType] = null;
-        }
-        if (newLevels[pollenType] !== null) {
-          isSelectedPollenType = true;
-        }
-      }
-
-      return isSelectedPollenType ? { ...record, levels: newLevels } : null;
-    })
+    .map((record) =>
+      applyLevelFilter(record, (pollenType) =>
+        selectedPollenTypes.includes(pollenType),
+      ),
+    )
     .filter((record): record is PollenRecord => record !== null);
 
   return {
