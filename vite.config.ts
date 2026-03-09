a898d3e4c43b8ce87981bab88227db2b0169a1f9
Jake Wilson <hi@jkwlsn.dev>
2026-03-09 13:35:14 +0000

    Add PWA assets to app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'snot.app',
+				short_name: 'snot.app',
+				display: 'minimal-ui',
+				dir: 'ltr',
+				lang: 'en',
+				start_url: '/',
+				scope: '/',
+				id: 'snot-app',
+				orientation: 'portrait',
+				theme_color: 'mediumseagreen',
+				background_color: 'mediumspringgreen',
+				icons: [
+					{ src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+					{ src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+					{ src: 'pwa-64x64.png', sizes: '64x64', type: 'image/png' },
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					}
+				]
+			},
+			workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'] }
+		})
+	],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
