f5a25580fa84b705c2c66d9a4459ae81b10c7ee5
Jake Wilson <hi@jkwlsn.dev>
2026-03-09 13:56:55 +0000

    Adds interfaces and types
    - Location coordinates
    - Resolved location (string name and coords)
    - Human-readable pollen risk level
    - Environment snapshots
    - Symptom entries
    - Basic k:v interface for app settings

---
+++ src/lib/types/index.ts
@@ -0,0 +1,55 @@
+// Interfaces and types for the app
+
+// Used for location data
+export interface LatLong {
+	lat: number;
+	long: number;
+}
+
+// Describes where teh value came from, web location or manual
+export type LocationSource = 'gps' | 'manual';
+
+export interface ResolvedLocation extends LatLong {
+	label: string;
+	source: LocationSource;
+}
+
+// User-readable pollen levels
+export type PollenRisk = 'low' | 'moderate' | 'high' | 'extreme';
+
+// Log the environment data for each symptom entry
+export interface EnvironmentAtLog {
+	timestamp: Date;
+	source: string;
+	pollenGrass: number | null;
+	pollenTree: number | null;
+	pollenWeed: number | null;
+	temperature: number | null;
+	humidity: number | null;
+	windSpeed: number | null;
+	pollenRiskTotal: PollenRisk;
+}
+
+// Symptoms can be rated between 0-5 in severity
+export type SymptomSeverity = 0 | 1 | 2 | 3 | 4 | 5;
+
+// Describes an entry in the symptom log
+// Describes timestamp, location, location name, areas affected and severity, and a snapshot of the environment at the moment of logging.
+export interface SymptomEntry {
+	id: number;
+	timestamp: Date;
+	location: LatLong;
+	locationLabel: string;
+	eyes: SymptomSeverity;
+	nose: SymptomSeverity;
+	throat: SymptomSeverity;
+	breathing: SymptomSeverity;
+	environment: EnvironmentAtLog | null;
+}
+
+// Basic K:V interface for app settings
+export interface AppSettings {
+	id: number;
+	key: string;
+	value: unknown;
+}
