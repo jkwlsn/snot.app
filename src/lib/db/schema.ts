b94135cea45af87b18a9e4798a2fef52904e7edb
Jake Wilson <hi@jkwlsn.dev>
2026-03-09 14:10:52 +0000

    Add `Dexie` schema
    Add tables for symptoms and settings

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,16 @@
+import Dexie, { type Table } from 'dexie';
+import type { SymptomEntry, AppSettings } from '$lib/types';
+
+export class SnotDB extends Dexie {
+	symptoms!: Table<SymptomEntry>;
+	settings!: Table<AppSettings>;
+
+	constructor() {
+		super('snot-app');
+		this.version(1).stores({
+			symptoms: '++id, timestamp'
+		});
+	}
+}
+
+export const db = new SnotDB();
