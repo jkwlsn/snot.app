19d3f7500062e18e87d2da9593dba1fff0cb4386
Jake Wilson <hi@jkwlsn.dev>
2026-03-10 09:26:46 +0000

    Add PWA meta tags to app `head`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import pwaIcon from '$lib/assets/pwa-icon-sneeze.svg';
-	import appleTouchIcon from '$lib/assets/apple-touch-icon-180x180.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<link rel="icon" href={pwaIcon} sizes="any" type="image/svg+xml" />
-	<link rel="apple-touch-icon" href={appleTouchIcon} />
+	<title>My Awesome App</title>
+	<meta name="description" content="My Awesome App description" />
 </svelte:head>
 
 {@render children()}
