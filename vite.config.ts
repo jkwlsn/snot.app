98765277a435dff8aecbab68c033e4a6706bee72
Jake Wilson <hi@jkwlsn.dev>
2026-03-10 09:25:27 +0000

    Update PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -57,19 +57,18 @@ export default defineConfig({
 			},
 			manifest: {
 				name: 'snot.app',
-				short_name: 'snot.app',
+				short_name: 'snot',
+				theme_color: 'mediumseagreen',
+				background_color: 'mediumspringgreen',
 				display: 'minimal-ui',
 				dir: 'ltr',
 				lang: 'en',
 				start_url: '/',
 				scope: '/',
-				id: 'snot-app',
 				orientation: 'portrait',
-				theme_color: 'mediumseagreen',
-				background_color: 'mediumspringgreen',
 				icons: [
 					{ src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-					{ src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+					{ src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
 					{ src: 'pwa-64x64.png', sizes: '64x64', type: 'image/png' },
 					{
 						src: 'maskable-icon-512x512.png',
