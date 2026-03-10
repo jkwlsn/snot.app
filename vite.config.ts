26e5b25f082bfcf32afcf286f84233e770d3ff91
Jake Wilson <hi@jkwlsn.dev>
2026-03-10 09:21:05 +0000

    Add `workbox` service worker options to `vite.config.ts`
    - Enable service worker in dev
    - Auto inject service worker
    - Auto update service worker
    - Cache js, css, html, ico,png, svg, and woff2 files
    - Use different strategies for different files: script, style, worker
      use stale while revalidate; use images from cache then network; use
      json from network then cache.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,54 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		SvelteKitPWA({
+			devOptions: { enabled: true },
 			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+			workbox: {
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+				skipWaiting: true,
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+				runtimeCaching: [
+					{
+						urlPattern: ({ request }) =>
+							request.destination === 'style' ||
+							request.destination === 'script' ||
+							request.destination === 'worker',
+						handler: 'StaleWhileRevalidate',
+						options: {
+							cacheName: 'static-resources',
+							expiration: {
+								maxEntries: 50,
+								maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+							}
+						}
+					},
+					{
+						urlPattern: ({ request }) => request.destination === 'image',
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'images',
+							expiration: {
+								maxEntries: 100,
+								maxAgeSeconds: 60 * 24 * 60 * 60 // 60 days
+							}
+						}
+					},
+					{
+						urlPattern: /\.json$/i,
+						handler: 'NetworkFirst',
+						options: {
+							cacheName: 'static-data-assets',
+							expiration: {
+								maxEntries: 32,
+								maxAgeSeconds: 24 * 60 * 60 // 24 hours
+							}
+						}
+					}
+				]
+			},
 			manifest: {
 				name: 'snot.app',
 				short_name: 'snot.app',
@@ -31,8 +78,7 @@ export default defineConfig({
 						purpose: 'maskable'
 					}
 				]
-			},
-			workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'] }
+			}
 		})
 	],
 	test: {
