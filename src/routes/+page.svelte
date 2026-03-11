65c4624292d1bfd74e3a5bcfa2ee680900372ae7
Jake Wilson <hi@jkwlsn.dev>
2026-03-11 10:52:52 +0000

    Refactor main route with component props and error handling
    - Move error handling to main route
    - Import data from symptom store
    - Pass data as props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
-<script>
+<script lang="ts">
+	import { symptoms, state } from '$lib/stores/symptoms.svelte'; // Dexie returns stores, not runes, reference these with `$`!
 	import SymptomForm from '$lib/components/SymptomForm.svelte';
 	import SymptomTable from '$lib/components/SymptomTable.svelte';
 </script>
 
 <h1>Snot.app</h1>
 <SymptomForm />
-<SymptomTable />
+{#if state.error}
+	<p>Failed to load symptoms: {state.error.message}</p>
+{:else if $symptoms === undefined}
+	<p>Loading...</p>
+{:else if $symptoms.length === 0}
+	<p>No data recorded.</p>
+{:else}
+	<SymptomTable symptoms={$symptoms} />
+{/if}
