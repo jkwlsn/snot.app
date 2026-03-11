147d8ef1a09b5e3eb3dc7020a4ff98c0e6c88713
Jake Wilson <hi@jkwlsn.dev>
2026-03-11 09:32:25 +0000

    Add `SymptomTable` component to main route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import SymptomForm from '$lib/components/SymptomForm.svelte';
+	import SymptomTable from '$lib/components/SymptomTable.svelte';
 </script>
 
 <h1>Snot.app</h1>
 <SymptomForm />
+<SymptomTable />
