259e3fa2a30acd6fb1fb2791de533ff2ad91ee8d
Jake Wilson <hi@jkwlsn.dev>
2026-03-17 12:46:19 +0000

    Add logging to browserGeolocationProvider

--- src/lib/location/providers/browserGeolocation.ts
+++ src/lib/location/providers/browserGeolocation.ts
@@ -1,15 +1,26 @@
+import type { Logger } from '$lib/logging';
 import type { GeolocationProvider } from '../types';
 
-export const browserGeolocationProvider = (): GeolocationProvider => ({
+const CONTEXT = { module: 'location', function: 'browserGeolocationProvider' };
+
+export const browserGeolocationProvider = (logger: Logger): GeolocationProvider => ({
 	async getCurrentPosition() {
 		return new Promise((resolve, reject) => {
+			logger.debug('Attempting to get current position', { ...CONTEXT });
 			navigator.geolocation.getCurrentPosition(
-				(position) =>
-					resolve({
+				(position) => {
+					const coordinates = {
 						latitude: position.coords.latitude,
 						longitude: position.coords.longitude
-					}),
-				reject
+					};
+					logger.debug('Successfully got position', { ...CONTEXT, coordinates });
+					resolve(coordinates);
+				},
+				(err) => {
+					const error = err instanceof Error ? err : new Error(String(err));
+					logger.error('Failed to get position', { ...CONTEXT, error });
+					reject(new Error(err.message));
+				}
 			);
 		});
 	}
