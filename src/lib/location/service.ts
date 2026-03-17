16169100395e29493a95fda6b08810c7296fd1e3
Jake Wilson <hi@jkwlsn.dev>
2026-03-17 14:40:45 +0000

    Add logging to location service

--- src/lib/location/service.ts
+++ src/lib/location/service.ts
@@ -1,3 +1,4 @@
+import type { Logger } from '$lib/logging';
 import type {
 	GeocodeProvider,
 	GeolocationProvider,
@@ -5,33 +6,93 @@ import type {
 	LocationService
 } from './types';
 
+const CONTEXT = { module: 'location', function: 'createLocationService' };
+
 export const createLocationService = ({
 	geocode,
-	geolocation
+	geolocation,
+	logger
 }: {
 	geocode: GeocodeProvider;
 	geolocation: GeolocationProvider;
+	logger: Logger;
 }): LocationService => {
 	const getBrowserLocation = async () => {
+		logger.debug('Requesting GPS coordinates', { ...CONTEXT, function: 'getBrowserLocation' });
 		const coordinates = await geolocation.getCurrentPosition();
 
-		if (!coordinates) return null;
+		if (!coordinates) {
+			logger.warn('GPS returned null', {
+				...CONTEXT,
+				function: 'getBrowserLocation'
+			});
+			return null;
+		}
+
+		logger.debug('Received coordinates', {
+			...CONTEXT,
+			function: 'getBrowserLocation',
+			coordinates
+		});
 
 		const location = await geocode.reverse(coordinates);
 
-		if (!location) return null;
+		if (!location) {
+			logger.warn('Reverse Geocode returned null', {
+				...CONTEXT,
+				function: 'getBrowserLocation',
+				coordinates
+			});
+			return null;
+		}
+
+		logger.info('Location resolved', {
+			...CONTEXT,
+			function: 'getBrowserLocation',
+			location
+		});
 
 		return location;
 	};
 
 	const forwardGeocode = async (query: string) => {
-		return await geocode.forward(query);
+		logger.debug('Attempting forward geocode', {
+			...CONTEXT,
+			function: 'forwardGeocode',
+			data: query
+		});
+		const results = await geocode.forward(query);
+		logger.debug('Forward geocode results', {
+			...CONTEXT,
+			function: 'forwardGeocode',
+			data: results
+		});
+		return results;
 	};
 
 	const reverseGeocode = async (coordinates: LocationCoordinates) => {
+		logger.debug('Attempting reverse geocode', {
+			...CONTEXT,
+			function: 'reverseGeocode',
+			data: coordinates
+		});
+
 		const location = await geocode.reverse(coordinates);
 
-		if (!location) return null;
+		if (!location) {
+			logger.debug('Reverse Geocode returned null', {
+				...CONTEXT,
+				function: 'reverseGeocode',
+				data: location
+			});
+			return null;
+		}
+
+		logger.debug('Reverse geocode results', {
+			...CONTEXT,
+			function: 'reverseGeocode',
+			data: location
+		});
 
 		return location;
 	};
