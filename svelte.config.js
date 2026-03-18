a95428d488910e6cc47f3aeca83cda8fc33eb601
Jake Wilson <hi@jkwlsn.dev>
2026-03-18 12:29:07 +0000

    fixup svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
-
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
@@ -11,8 +9,7 @@ const config = {
 			$lib: 'src/lib',
 			$db: 'src/lib/db',
 			$types: 'src/lib/types/'
-		},
-		paths: { base: dev ? '' : '/snot.app' }
+		}
 	}
 };
 
