76a86dae6e99181c828bf8a3cee6f5c122110c4d
Jake Wilson <hi@jkwlsn.dev>
2026-03-19 17:13:43 +0000

    Add types for stacked bar graph

--- src/lib/graphs/types.ts
+++ src/lib/graphs/types.ts
@@ -1,3 +1,5 @@
+// Temporal graph interfaces
+import type { SymptomName } from '$lib/config';
 import type { SymptomRecord } from '$lib/types';
 
 export interface TemporalDataPoint {
@@ -7,4 +9,13 @@ export interface TemporalDataPoint {
 
 export interface TemporalGraph {
 	transform(records: SymptomRecord[]): TemporalDataPoint[];
+}
+
+// Stacked bar graph data
+export type StackedBarDataPoint = {
+	timestamp: Date;
+} & Record<SymptomName, number>;
+
+export interface StackedBarGraph {
+	transform(records: SymptomRecord[]): StackedBarDataPoint[];
 }
