36f64a2f376f11599638833bda8c20dff0e1e9d3
Jake Wilson <hi@jkwlsn.dev>
2026-03-19 14:32:04 +0000

    Adds `SymptomCalendarGraph` to route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import SymptomForm from '$lib/components/SymptomForm.svelte';
 	import SymptomTable from '$lib/components/SymptomTable.svelte';
 	import LocationInput from '$lib/components/LocationInput.svelte';
+	import SymptomCalendarGraph from '$lib/components/SymptomCalendarGraph.svelte';
 </script>
 
 <h1>Snot.app</h1>
@@ -16,4 +17,5 @@
 	<p>No data recorded.</p>
 {:else}
 	<SymptomTable symptoms={$symptoms} />
+	<SymptomCalendarGraph graphTitle="Symptoms per day" records={$symptoms} />
 {/if}
