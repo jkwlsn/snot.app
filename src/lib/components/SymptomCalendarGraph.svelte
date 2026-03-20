ff22afce39a1fe1e77c386e53bd4762869ac1245
Jake Wilson <hi@jkwlsn.dev>
2026-03-20 14:17:32 +0000

    Add conditional display to component

--- src/lib/components/SymptomCalendarGraph.svelte
+++ src/lib/components/SymptomCalendarGraph.svelte
@@ -7,11 +7,11 @@
 	import type { SymptomRecord } from '$lib/types';
 
 	type Props = {
-		graphTitle: string;
+		title: string;
 		records: SymptomRecord[];
 	};
 
-	let { records, graphTitle }: Props = $props();
+	let { records, title }: Props = $props();
 
 	const now = new Date();
 	const start = startOfYear(now);
@@ -26,60 +26,66 @@
 </script>
 
 <section>
-	<h2>{graphTitle}</h2>
-	<Chart
-		data={chart.data}
-		x="timestamp"
-		c="value"
-		cScale={scaleThreshold()}
-		cDomain={chart.cDomain}
-		cRange={[
-			'oklch(90% 0.15 100)',
-			'oklch(78% 0.18 60)',
-			'oklch(62% 0.20 35)',
-			'oklch(48% 0.22 20)'
-		]}
-		padding={{ top: 20 }}
-		height={140}
-	>
-		{#snippet children({ context })}
-			<Layer>
-				<Calendar {start} {end}>
-					{#snippet children({ cells, cellSize })}
-						{#each cells as cell, i (i)}
-							{@const padding = 1}
-							<Rect
-								x={cell.x + padding}
-								y={cell.y + padding}
-								width={cellSize[0] - padding * 2}
-								height={cellSize[1] - padding * 2}
-								rx={4}
-								fill={cell.color ?? 'rgb(0 0 0 / 5%)'}
-								onpointermove={(e) => context.tooltip?.show(e, cell.data)}
-								onpointerleave={() => context.tooltip?.hide()}
-							/>
-						{/each}
-					{/snippet}
-				</Calendar>
-			</Layer>
+	<h2>{title}</h2>
+	{#if records?.length === 0}
+		<p>No data</p>
+	{:else if records === undefined}
+		<p>Loading</p>
+	{:else}
+		<Chart
+			data={chart.data}
+			x="timestamp"
+			c="value"
+			cScale={scaleThreshold()}
+			cDomain={chart.cDomain}
+			cRange={[
+				'oklch(90% 0.15 100)',
+				'oklch(78% 0.18 60)',
+				'oklch(62% 0.20 35)',
+				'oklch(48% 0.22 20)'
+			]}
+			padding={{ top: 20 }}
+			height={140}
+		>
+			{#snippet children({ context })}
+				<Layer>
+					<Calendar {start} {end}>
+						{#snippet children({ cells, cellSize })}
+							{#each cells as cell, i (i)}
+								{@const padding = 1}
+								<Rect
+									x={cell.x + padding}
+									y={cell.y + padding}
+									width={cellSize[0] - padding * 2}
+									height={cellSize[1] - padding * 2}
+									rx={4}
+									fill={cell.color ?? 'rgb(0 0 0 / 5%)'}
+									onpointermove={(e) => context.tooltip?.show(e, cell.data)}
+									onpointerleave={() => context.tooltip?.hide()}
+								/>
+							{/each}
+						{/snippet}
+					</Calendar>
+				</Layer>
 
-			<Tooltip.Root>
-				{#snippet children({ data })}
-					{#if data?.value != null}
-						<Tooltip.Header>{format(data.timestamp, PeriodType.Day)}</Tooltip.Header>
-						<Tooltip.List>
-							<Tooltip.Item
-								label="Symptoms"
-								value={data.value}
-								format="integer"
-								valueAlign="right"
-							/>
-						</Tooltip.List>
-					{:else}
-						No Data
-					{/if}
-				{/snippet}
-			</Tooltip.Root>
-		{/snippet}
-	</Chart>
+				<Tooltip.Root>
+					{#snippet children({ data })}
+						{#if data?.value != null}
+							<Tooltip.Header>{format(data.timestamp, PeriodType.Day)}</Tooltip.Header>
+							<Tooltip.List>
+								<Tooltip.Item
+									label="Symptoms"
+									value={data.value}
+									format="integer"
+									valueAlign="right"
+								/>
+							</Tooltip.List>
+						{:else}
+							No Data
+						{/if}
+					{/snippet}
+				</Tooltip.Root>
+			{/snippet}
+		</Chart>
+	{/if}
 </section>
