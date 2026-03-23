1b6313361279d5d5dc4d4270427e1e6188d2fafa
Jake Wilson <hi@jkwlsn.dev>
2026-03-23 15:13:34 +0000

    Changes `state` to `appState`
    `state` was clashing with the `$state` svelte rune.

--- src/lib/stores/symptoms.svelte.ts
+++ src/lib/stores/symptoms.svelte.ts
@@ -1,13 +1,13 @@
 import { liveQuery } from 'dexie';
 import { symptomService as service } from '$lib/services';
 
-export const state = $state<{ error: Error | null }>({ error: null });
+export const appState = $state<{ error: Error | null }>({ error: null });
 
 export const symptoms = liveQuery(async () => {
 	try {
 		return await service.getAllSymptoms();
 	} catch (e) {
-		state.error = e instanceof Error ? e : new Error(String(e));
+		appState.error = e instanceof Error ? e : new Error(String(e));
 		return [];
 	}
 });
