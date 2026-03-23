d77638a623c9c9412fdc90fccdce82910dddf2ea
Jake Wilson <hi@jkwlsn.dev>
2026-03-23 15:45:15 +0000

    Add precomputed `todaysData` to stores

--- src/lib/stores/symptoms.svelte.ts
+++ src/lib/stores/symptoms.svelte.ts
@@ -1,5 +1,6 @@
 import { liveQuery } from 'dexie';
 import { symptomService as service } from '$lib/services';
+import { endOfDay, startOfDay } from 'date-fns';
 
 export const appState = $state<{ error: Error | null }>({ error: null });
 
@@ -8,6 +9,16 @@ export const symptoms = liveQuery(async () => {
 		return await service.getAllSymptoms();
 	} catch (e) {
 		appState.error = e instanceof Error ? e : new Error(String(e));
+		return [];
+	}
+});
+
+export const todaysSymptoms = liveQuery(async () => {
+	try {
+		// eslint-disable-next-line svelte/prefer-svelte-reactivity
+		return await service.getRangeSymptoms(startOfDay(new Date()), endOfDay(new Date()));
+	} catch (err) {
+		appState.error = err instanceof Error ? err : new Error(String(err));
 		return [];
 	}
 });
