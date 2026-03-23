992d52ce05a526706592033b158d006bc8c168c2
Jake Wilson <hi@jkwlsn.dev>
2026-03-23 15:13:34 +0000

    Changes `state` to `appState`
    `state` was clashing with the `$state` svelte rune.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { symptoms, state } from '$lib/stores/symptoms.svelte'; // Dexie returns stores, not runes, reference these with `$`!
+	import { symptoms, appState } from '$lib/stores/symptoms.svelte'; // Dexie returns stores, not runes, reference these with `$`!
 	import SymptomForm from '$lib/components/SymptomForm.svelte';
 	import SymptomTable from '$lib/components/SymptomTable.svelte';
 	import LocationInput from '$lib/components/LocationInput.svelte';
@@ -9,8 +9,8 @@
 <h1>Snot.app</h1>
 <LocationInput />
 <SymptomForm />
-{#if state.error}
-	<p>Failed to load symptoms: {state.error.message}</p>
+{#if appState.error}
+	<p>Failed to load symptoms: {appState.error.message}</p>
 {:else}
 	<SymptomCalendarGraph title="Symptom count per day" records={$symptoms} />
 	<SymptomTable title="Symptom Log" records={$symptoms} />
