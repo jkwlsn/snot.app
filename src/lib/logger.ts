ca1b51fab141065d7ed8678f465a75a6fe32b584
Jake Wilson <hi@jkwlsn.dev>
2026-03-25 23:50:29 +0000

    Delete redundant `lib/logger.ts`

--- src/lib/logger.ts
+++
@@ -1,7 +0,0 @@
-import { createLogger, consoleProvider, nullProvider } from '$lib/logging';
-import { dev } from '$app/environment';
-
-export const logger = createLogger(
-	import.meta.env.MODE === 'test' ? nullProvider : consoleProvider,
-	dev ? 'debug' : 'info'
-);
