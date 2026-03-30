a0827461be6aa6db88f4c912b74929f3e2a8c511
Jake Wilson <hi@jkwlsn.dev>
2026-03-30 14:25:50 +0100

    Delete unnecessary singleton export files

--- src/lib/db/repository/index.ts
+++ src/lib/db/repository/index.ts
@@ -1,9 +1,2 @@
-import { createSymptomRepository } from './symptoms';
-import { createLogger, consoleProvider } from '$lib/logging';
-
-const logger = createLogger(consoleProvider);
-
-export const symptomRepository = createSymptomRepository(logger);
-
 // Types
 export type { SymptomRepository } from './types';
--- src/lib/services/index.ts
+++
@@ -1,7 +0,0 @@
-import { createSymptomService } from './symptoms';
-import { createLogger, consoleProvider } from '$lib/logging';
-import { symptomRepository } from '$lib/db/repository';
-
-const logger = createLogger(consoleProvider);
-
-export const symptomService = createSymptomService(symptomRepository, logger);
