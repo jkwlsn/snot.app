529b8bd5fc79472d9e019d6637903caef57ec6c6
Jake Wilson <hi@jkwlsn.dev>
2026-03-30 16:42:40 +0100

    Adds `eslint-plugin-import-x` config
    Sets up typescript `eslint-plugin-import-x` rules.
    
    - Ensure imports are listed first
    - Ensure exports are listed last
    - Ensure no duplicate imports are used
    - Ensure packages are imported with aliases
    - Ensure newlines between imports and code
    - Set restrictions on where files can be imported from

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
-import prettier from 'eslint-config-prettier';
 import path from 'node:path';
+import prettier from 'eslint-config-prettier';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import ts from 'typescript-eslint';
+import { importX } from 'eslint-plugin-import-x';
 import svelteConfig from './svelte.config.js';
 
 const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
@@ -19,10 +20,47 @@ export default defineConfig(
 	...svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		plugins: { import: importX },
 		rules: {
-			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			'no-undef': 'off',
+			'@typescript-eslint/no-import-type-side-effects': 'error',
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			'@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+			'import/first': 'error',
+			'import/no-duplicates': 'error',
+			'import/no-relative-packages': 'error',
+			'import/newline-after-import': 'error',
+			complexity: ['warn', { max: 15 }],
+			'no-nested-ternary': 'error',
+			'no-restricted-syntax': [
+				'error',
+				{
+					selector: 'TSEnumDeclaration',
+					message: 'Use literal unions or `as const` objects instead of enums.'
+				}
+			],
+			'import/order': [
+				'error',
+				{
+					groups: [
+						'builtin',
+						'external',
+						'internal',
+						'parent',
+						'sibling',
+						'index',
+						'object',
+						'unknown',
+						'type'
+					]
+				}
+			]
+			//			'import/no-restricted-paths': [
+			//				'error',
+			//				{
+			//					zones: []
+			//				}
+			//			]
 		}
 	},
 	{
