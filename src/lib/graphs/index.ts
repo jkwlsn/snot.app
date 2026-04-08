5f3367971bd86c4813b85029f4b4db2c9667b021
Jake Wilson <hi@jkwlsn.dev>
2026-04-08 08:15:26 +0100

    Add `layerchartBarGraph` provider
    Converts a list of `SymptomRecord` into a list of `LabelledDataPoint`

--- src/lib/graphs/index.ts
+++ src/lib/graphs/index.ts
@@ -1,3 +1,4 @@
 export { createLayerchartCalendarGraph } from './providers/layerchartCalendarGraph';
 export { createLayerchartRadarGraph } from './providers/layerchartRadarGraph';
 export { createLayerchartStackedBarGraph } from './providers/layerchartStackedBarGraph';
+export { createLayerchartBarGraph } from './providers/layerchartBarGraph';
