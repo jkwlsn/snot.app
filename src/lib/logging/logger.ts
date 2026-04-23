7442878119852e39eaceb829fa5ea02d632208f6
Jake Wilson <hi@jkwlsn.dev>
2026-04-23 15:10:03 +0100

    Move `error` out of `context` in `logger.error`

--- src/lib/logging/logger.ts
+++ src/lib/logging/logger.ts
@@ -12,9 +12,8 @@ export function createLogger(provider: LogProvider, minLevel: LogLevel = 'info')
 		debug: (message: string, context?: LogEntry['context']) => emit('debug', message, context),
 		info: (message: string, context?: LogEntry['context']) => emit('info', message, context),
 		warn: (message: string, context?: LogEntry['context']) => emit('warn', message, context),
-		error: (message: string, context?: LogEntry['context'] & { error: Error }) => {
-			const { error, ...rest } = context ?? {};
-			emit('error', message, Object.keys(rest).length ? rest : undefined, error);
+		error: (message: string, error?: Error, context?: LogEntry['context']) => {
+			emit('error', message, context, error);
 		}
 	};
 }
