2b06f36b15a856d3ac40591a4e8a58d0685e4902
Jake Wilson <hi@jkwlsn.dev>
2026-04-23 12:37:08 +0100

    Refactor `types/index.ts`
    Splits up `index.ts` into various files. `index` is now a barrel file.

---
+++ src/lib/types/repository.ts
@@ -0,0 +1,9 @@
+import type { WithId, StoredId } from './base';
+
+export interface Repository<TInput, TOutput extends WithId> {
+	add(entry: TInput): Promise<StoredId>;
+	update(id: StoredId, patch: Partial<TInput>): Promise<void>;
+	remove(id: StoredId): Promise<void>;
+	getById(id: StoredId): Promise<TOutput | undefined>;
+	getAll(): Promise<TOutput[]>;
+}
