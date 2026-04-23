03c3abe9e7cef4eb90d8f75472c5332f1040e71a
Jake Wilson <hi@jkwlsn.dev>
2026-04-23 11:49:53 +0100

    Add `SymptomFields` to `SymptomService.submitSymptoms`

--- src/lib/services/types.ts
+++ src/lib/services/types.ts
@@ -1,7 +1,7 @@
-import type { SymptomLog, UserLocation } from '$lib/types';
+import type { SymptomFields, SymptomLog, UserLocation } from '$lib/types';
 
 export interface SymptomService {
-	submitSymptoms(values: Record<string, number>, location: UserLocation | null): Promise<number>;
+	submitSymptoms(values: SymptomFields, location: UserLocation | null): Promise<number>;
 	getAllSymptoms(): Promise<SymptomLog[]>;
 	getRangeSymptoms(from: Date, to: Date): Promise<SymptomLog[]>;
 	removeSymptom(id: number): Promise<void>;
