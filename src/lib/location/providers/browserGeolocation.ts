efe8e6b108dc6d95c912fe344a55ef5959419682
Jake Wilson <hi@jkwlsn.dev>
2026-04-23 14:59:09 +0100

    Remove logger from `browserGeolocationProvider`

--- src/lib/location/providers/browserGeolocation.ts
+++ src/lib/location/providers/browserGeolocation.ts
@@ -1,24 +1,16 @@
-import type { Logger } from '$lib/logging';
 import type { GeolocationProvider } from '$lib/types';
 
-const CONTEXT = { module: 'location', function: 'browserGeolocationProvider' };
-
-export const browserGeolocationProvider = (logger: Logger): GeolocationProvider => ({
+export const browserGeolocationProvider = (): GeolocationProvider => ({
 	async getCurrentPosition() {
 		return new Promise((resolve, reject) => {
-			logger.debug('Attempting to get current position', { ...CONTEXT });
 			navigator.geolocation.getCurrentPosition(
 				(position) => {
-					const coordinates = {
+					resolve({
 						latitude: position.coords.latitude,
 						longitude: position.coords.longitude
-					};
-					logger.debug('Successfully got position', { ...CONTEXT, coordinates });
-					resolve(coordinates);
+					});
 				},
 				(err) => {
-					const error = err instanceof Error ? err : new Error(String(err));
-					logger.error('Failed to get position', { ...CONTEXT, error });
 					reject(new Error(err.message));
 				}
 			);
