ea044f50c69b4dac37debacc47a4db8951067b16
Jake Wilson <hi@jkwlsn.dev>
2026-04-23 13:31:37 +0100

    Import from absolute `$lib/types`
    Remove local and relative imports and import directly from `$lib/types`.

--- src/lib/logging/index.ts
+++ src/lib/logging/index.ts
@@ -1,5 +1,4 @@
 export { createLogger } from './logger';
 export { nullProvider } from './providers/null';
 export { consoleProvider } from './providers/console';
-export type { LogLevel, LogEntry, LogProvider } from './types';
-export type { Logger } from './logger';
+export type { LogLevel, LogEntry, LogProvider, Logger } from '$lib/types';
--- src/lib/services/types.ts
+++
@@ -1,8 +0,0 @@
-import type { SymptomLog, UserLocation } from '$lib/types';
-
-export interface SymptomService {
-	submitSymptoms(values: Record<string, number>, location: UserLocation | null): Promise<number>;
-	getAllSymptoms(): Promise<SymptomLog[]>;
-	getRangeSymptoms(from: Date, to: Date): Promise<SymptomLog[]>;
-	removeSymptom(id: number): Promise<void>;
-}
