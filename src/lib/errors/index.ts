82e5d8d13d6a0e69e6bcef0338af48989bf9c736
Jake Wilson <hi@jkwlsn.dev>
2026-04-24 18:31:58 +0100

    Add `index` file to `error` module

---
+++ src/lib/errors/index.ts
@@ -0,0 +1,4 @@
+export { handleError } from './handleError';
+export { clearAppError, setAppError, appErrorState } from './errorState.svelte';
+export * from './types';
+export { default as ErrorDisplay } from './components/ErrorDisplay.svelte';
