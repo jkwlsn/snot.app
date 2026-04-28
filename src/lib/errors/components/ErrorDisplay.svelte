853f3425ac6d15563a5ae70af849358f11ea4938
Jake Wilson <hi@jkwlsn.dev>
2026-04-28 17:17:45 +0100

    Add error handling component
    This component handles the display of errors in the UI.
    
    It imports the `appErrorState` and shows the current error, if it
    exists.

---
+++ src/lib/errors/components/ErrorDisplay.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { clearAppError, appErrorState } from '$lib/errors';
+
+	function dismissError() {
+		clearAppError();
+	}
+</script>
+
+{#if appErrorState.error}
+	{@const currentError = appErrorState.error}
+	<div>
+		<p><strong>Error {currentError.code}:</strong> {currentError.message}</p>
+		{#if currentError.details}
+			<pre>{JSON.stringify(currentError.details, null, 2)}</pre>
+		{/if}
+		<button on:click={dismissError}>X</button>
+	</div>
+{/if}
