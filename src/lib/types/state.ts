67cc9f5a3b493273325489c17c8da6b44740b941
Jake Wilson <hi@jkwlsn.dev>
2026-04-30 15:55:11 +0100

    Move `symptom` types and config to `symptoms` domain
    - Moves symptoms types to `symptoms/types`
    - Moves symptom config to `symptoms/config`
    - Updates `index` exports

--- src/lib/types/state.ts
+++ src/lib/types/state.ts
@@ -1,11 +1,6 @@
-import type { UserLocation, SymptomLog } from './index';
+import type { UserLocation } from './index';
 
 export interface LocationState {
 	currentLocation: UserLocation | null;
 	searchResults: UserLocation[];
-}
-
-export interface SymptomState {
-	readonly symptoms: SymptomLog[];
-	readonly todaysSymptoms: SymptomLog[];
 }
