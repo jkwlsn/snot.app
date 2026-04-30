600d9c9bd8be8b2c10ba323095a540cd0e5b851c
Jake Wilson <hi@jkwlsn.dev>
2026-04-30 16:56:59 +0100

    Move symptom graphs and graph providers to `symptom` domain
    Moves the symptom-related graphs to the symptom domain.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	import { getSymptomState } from '$lib/symptoms';
-	import SymptomForm from '$lib/components/SymptomForm.svelte';
-	import SymptomTable from '$lib/components/SymptomTable.svelte';
+	import {
+		getSymptomState,
+		SymptomForm,
+		SymptomTable,
+		SymptomBarGraph,
+		SymptomCalendarGraph
+	} from '$lib/symptoms';
 	import LocationInput from '$lib/components/LocationInput.svelte';
-	import SymptomCalendarGraph from '$lib/components/SymptomCalendarGraph.svelte';
-	import SymptomBarGraph from '$lib/components/SymptomBarGraph.svelte';
 	import { ErrorDisplay } from '$lib/errors';
 
 	const records = getSymptomState();
