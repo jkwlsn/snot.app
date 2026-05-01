efcf5c887138ad08a382f98cb3f400696d75a7bc
Jake Wilson <hi@jkwlsn.dev>
2026-05-01 10:34:18 +0100

    Move location-related types to `location` module
    Moves from a layera architecture to domain-driven architecture.
    Updates imports of types in related files.

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,7 +1,5 @@
 export * from './base';
-export * from './location';
 export * from './environment';
 export * from './repository';
 export * from './settings';
 export * from './graphs';
-export * from './state';
