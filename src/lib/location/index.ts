95053fe40e4ddde76003f1c5e5fabfce73f0fa31
Jake Wilson <hi@jkwlsn.dev>
2026-05-01 10:38:18 +0100

    Move `LocationInput` component to `location` module
    Part of the move from a layer architecture to a domain-driven architecture.
    Updates exports and imports.

--- src/lib/location/index.ts
+++ src/lib/location/index.ts
@@ -3,4 +3,5 @@ export { locationState } from './locationState.svelte';
 export { getLocationService, setLocationService } from './locationContext';
 export { browserGeolocationProvider } from './providers/browserGeolocation';
 export { nominatimGeocodeProvider } from './providers/nominatimGeocodeProvider';
+export { default as LocationInput } from './components/LocationInput.svelte';
 export * from './types';
