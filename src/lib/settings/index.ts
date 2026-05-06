efd2a724fea7a5ce0c3936f89389aa6ee03cb2a0
Jake Wilson <hi@jkwlsn.dev>
2026-05-06 16:24:00 +0100

    Add `SettingsInput` component to `settings` module

--- src/lib/settings/index.ts
+++ src/lib/settings/index.ts
@@ -4,6 +4,7 @@ export {
 	setSettingsContext,
 	getSettingsContext
 } from './settingsState.svelte';
+export { default as SettingsForm } from './components/SettingsForm.svelte';
 export type {
 	Theme,
 	AppSettings,
