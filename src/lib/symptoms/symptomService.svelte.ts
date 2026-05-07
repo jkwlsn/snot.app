a6c37867773e069d2a82e90d3c89cdcb947569fb
Jake Wilson <hi@jkwlsn.dev>
2026-05-07 11:09:31 +0100

    Add logic to handle `locationEnabled` setting
    The `settings` module currently features `locationEnabled`. This
    controls whether the user location should be displayed and submitted
    with symptom data.
    
    This commit adds checks to the `LocationInput` component to hide
    location input if it is disabled in settings.
    
    If location is enabled in settings but permission to use the browser
    API is denied then a `isGeolocationPermissionError` error is raised.
    
    In the case that permission is granted but location is disabled, a check
    in the symptom service prevents location data being added to the
    database.
    
    Only when `locationEnabled` is true and permission is granted to use the
    browser geolocation API can the current location be passed to the symptom
    repository.

--- src/lib/symptoms/symptomService.svelte.ts
+++ src/lib/symptoms/symptomService.svelte.ts
@@ -2,24 +2,29 @@ import { createSymptomRepository } from './symptomRepository';
 import type { SymptomFields, SymptomService } from '$lib/symptoms';
 import type { LocationState } from '$lib/location';
 import type { LoggingService } from '$lib/logging';
+import type { SettingsService } from '$lib/settings';
 
 export function createSymptomService({
 	logger,
-	locationState
+	locationState,
+	settingsService
 }: {
 	logger: LoggingService;
 	locationState: LocationState;
+	settingsService: SettingsService;
 }): SymptomService {
 	const repo = createSymptomRepository({ logger });
 
 	return {
-		submitSymptoms: (values: SymptomFields) =>
-			repo.add({
+		submitSymptoms: (values: SymptomFields) => {
+			const settings = settingsService.getSettings();
+			return repo.add({
 				// eslint-disable-next-line svelte/prefer-svelte-reactivity
 				createdAt: new Date(),
-				location: $state.snapshot(locationState.currentLocation),
+				location: settings.locationEnabled ? $state.snapshot(locationState.currentLocation) : null,
 				symptoms: values
-			}),
+			});
+		},
 		getAllSymptoms: () => repo.getAll(),
 		getRangeSymptoms: (from: Date, to: Date) => repo.getRange(from, to),
 		removeSymptom: (id: number) => repo.remove(id)
