6521cf73dc82ebb28f9410376cf70a794a34e3f5
Jake Wilson <hi@jkwlsn.dev>
2026-05-12 11:42:08 +0100

    Add `get / set EnvironmentState` to `environment` module index file

--- src/lib/environment/index.ts
+++ src/lib/environment/index.ts
@@ -2,5 +2,7 @@ export { createEnvironmentService } from './environmentService';
 export {
 	setEnvironmentService,
 	getEnvironmentService,
+	setEnvironmentState,
+	getEnvironmentState
 } from './environmentContext';
 export { createEnvironmentState } from './environmentState.svelte.ts';
