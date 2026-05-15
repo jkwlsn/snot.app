f3944b861c6e507ea17c9c921d90530e99bd1e9f
Jake Wilson <hi@jkwlsn.dev>
2026-05-15 13:29:37 +0100

    Add `EnvironmentProvider` to `environment/types`

--- src/lib/environment/types.ts
+++ src/lib/environment/types.ts
@@ -1,5 +1,5 @@
 import type { CreatedAt } from '$lib/types';
-import type { WithLocation } from '$lib/location';
+import type { UserLocation, WithLocation } from '$lib/location';
 import type { POLLEN_UNITS, POLLENS, POLLEN_SEVERITY } from './config';
 
 export type PollenType = (typeof POLLENS)[number]['id'];
@@ -26,4 +26,18 @@ export interface PollenInstant extends CreatedAt {
 export interface PollenSeries extends CreatedAt, WithLocation {
 	pollenTypes: PollenType[];
 	instants: PollenInstant[];
+}
+
+export interface EnvironmentProvider<EnvironmentProviderResponse> {
+	readonly supportedPollens: PollenType[];
+	getCurrent(
+		pollenTypes: PollenType[],
+		location: UserLocation
+	): Promise<EnvironmentProviderResponse>;
+	getForecast(
+		pollenTypes: PollenType[],
+		location: UserLocation,
+		from: Date,
+		to: Date
+	): Promise<EnvironmentProviderResponse>;
 }
