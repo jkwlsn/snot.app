a1cdd2b92fb1b9709fad09c545c8edd56b58089a
Jake Wilson <hi@jkwlsn.dev>
2026-05-21 12:39:54 +0100

    Refactor `logging` module to use UTC dates

--- src/lib/logging/providers/console.ts
+++ src/lib/logging/providers/console.ts
@@ -1,11 +1,13 @@
+import { formatDisplayDate } from '$lib/date';
 import type { LogEntry, LogProvider } from '../types';
 
 export const consoleProvider: LogProvider = {
-	log: ({ level, message, context, error }: LogEntry) => {
+	log: ({ level, message, context, error, createdAt, timezone }: LogEntry) => {
+		const formattedDate = formatDisplayDate(createdAt, undefined, timezone);
 		const extras: unknown[] = [];
 		if (context) extras.push(context);
 		if (error) extras.push(error);
 
-		console[level](message, ...extras);
+		console[level](`[${formattedDate}] ${message}`, ...extras);
 	}
 };
