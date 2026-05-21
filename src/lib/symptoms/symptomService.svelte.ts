b9600435c1f461f54d9b8bceb5df09c12af5e6b9
Jake Wilson <hi@jkwlsn.dev>
2026-05-21 12:40:36 +0100

    Refactor `symptoms` module to use UTC dates
    All `Date`s are now `UTCDate`s. The repository now uses the new
    `Persisted` type to store dates in Dexie as UNIX timestamps (numbers).

--- src/lib/symptoms/symptomService.svelte.ts
+++ src/lib/symptoms/symptomService.svelte.ts
@@ -1,5 +1,7 @@
 import { createSymptomRepository } from './symptomRepository';
+import { getUTCNow } from '$lib/date';
 import type { SymptomFields, SymptomService } from '$lib/symptoms';
+import type { UTCDate } from '$lib/date';
 import type { LocationState } from '$lib/location';
 import type { LoggingService } from '$lib/logging';
 import type { SettingsService } from '$lib/settings';
@@ -19,14 +21,14 @@ export function createSymptomService({
 		submitSymptoms: (values: SymptomFields) => {
 			const settings = settingsService.getSettings();
 			return repo.add({
-				// eslint-disable-next-line svelte/prefer-svelte-reactivity
-				createdAt: new Date(),
+				createdAt: getUTCNow(),
+				timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
 				location: settings.locationEnabled ? $state.snapshot(locationState.currentLocation) : null,
 				symptoms: values
 			});
 		},
 		getAllSymptoms: () => repo.getAll(),
-		getRangeSymptoms: (from: Date, to: Date) => repo.getRange(from, to),
+		getRangeSymptoms: (from: UTCDate, to: UTCDate) => repo.getRange(from, to),
 		removeSymptom: (id: number) => repo.remove(id)
 	};
 }
