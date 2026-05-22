90fd08d4d9b76637b7b1d26d9c13c6de12d0c080
Jake Wilson <hi@jkwlsn.dev>
2026-05-22 16:47:16 +0100

    Add `/history` route
    Move `SymptomTable` component to `/history` route

---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { getSymptomState, SymptomTable } from '$lib/symptoms';
+	const symptomData = getSymptomState();
+</script>
+
+<h2>History</h2>
+<SymptomTable title="Symptom Log" records={symptomData.symptoms} />
