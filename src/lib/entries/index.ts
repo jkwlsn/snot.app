98110a55f08f5492a67527a4b6c8f47f4009d86b
Jake Wilson <hi@jkwlsn.dev>
2026-06-19 13:57:28 +0100

    Add `entryContext` file to `entries` module
    Copied from `symptoms` module.

--- src/lib/entries/index.ts
+++ src/lib/entries/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
 export { createEntryService } from './entryService.svelte';
 export { createEntryState } from './entryState.svelte';
+export { setEntryService, getEntryService, setEntryState, getEntryState } from './entryContext';
