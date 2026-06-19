69eb13055cadbf9c27693524cad2abd55f0fcb2d
Jake Wilson <hi@jkwlsn.dev>
2026-06-19 12:03:48 +0100

    Add Entry module types
    Adds types for the `Entry` module. This will handle logging pollen / symptom entries.
    This module prevents dependnecies directly between the `symptoms` and `environment` modules.
    
    Theses types have been largely copied from the `symptoms` types.

---
+++ src/lib/entries/types.ts
@@ -0,0 +1,26 @@
+import type { WithLocation } from '$lib/location';
+import type { SymptomFields } from '$lib/symptoms';
+import type { CreatedAt, Repository, Stored } from '$lib/types';
+import type { UTCDate } from '@date-fns/utc';
+
+export interface CreateEntry extends CreatedAt, WithLocation {
+	symptoms: SymptomFields;
+}
+
+export type Entry = Stored<CreateEntry>;
+
+export interface EntryRepository extends Repository<CreateEntry, Entry> {
+	getRange(from: UTCDate, to: UTCDate): Promise<Entry[]>;
+}
+
+export interface EntryService {
+	submitEntry(symptoms: SymptomFields): Promise<number>;
+	getAllEntries(): Promise<Entry[]>;
+	getRangeEntries(from: UTCDate, to: UTCDate): Promise<Entry[]>;
+	removeEntry(id: number): Promise<void>;
+}
+
+export interface EntryState {
+	readonly entries: Entry[];
+	readonly todaysEntries: Entry[];
+}
