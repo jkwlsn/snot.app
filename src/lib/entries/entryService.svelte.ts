4b7d24948ea5f894ff8ea5de782aa717d95ed282
Jake Wilson <hi@jkwlsn.dev>
2026-06-19 13:22:02 +0100

    Add `entryService` to `entries` module
    Adds the entry module service. This is copied from the `symptoms`
    module.

---
+++ src/lib/entries/entryService.svelte.ts
@@ -0,0 +1,35 @@
+import { getUTCNow } from '$lib/date';
+import type { EntryService } from '$lib/entries';
+import type { UTCDate } from '$lib/date';
+import type { LocationState } from '$lib/location';
+import type { LoggingService } from '$lib/logging';
+import type { SettingsService } from '$lib/settings';
+import { createEntryRepository } from './entryRepository';
+import type { SymptomFields } from '$lib/symptoms';
+
+export function createEntryService({
+	logger,
+	locationState,
+	settingsService
+}: {
+	logger: LoggingService;
+	locationState: LocationState;
+	settingsService: SettingsService;
+}): EntryService {
+	const repo = createEntryRepository({ logger });
+
+	return {
+		submitEntry: (symptoms: SymptomFields) => {
+			const settings = settingsService.getSettings();
+			return repo.add({
+				createdAt: getUTCNow(),
+				timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+				location: settings.locationEnabled ? $state.snapshot(locationState.currentLocation) : null,
+				symptoms: symptoms
+			});
+		},
+		getAllEntries: () => repo.getAll(),
+		getRangeEntries: (from: UTCDate, to: UTCDate) => repo.getRange(from, to),
+		removeEntry: (id: number) => repo.remove(id)
+	};
+}
