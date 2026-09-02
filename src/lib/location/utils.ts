533c8ef497c82f3a059ef415be696e95e21f2705
Jake Wilson <hi@jkwlsn.dev>
2026-09-02 12:52:54 +0100

    Add coordinate rounding functions to location utils
    Adds a function to round to a given precision. Also adds a function to
    round to 1km, which simply wraps the `roundToPrecision` function with a
    default value of 1km.

--- src/lib/location/utils.ts
+++ src/lib/location/utils.ts
@@ -5,3 +5,11 @@ export function isGeolocationPermissionError(err: unknown): err is Error & { cod
 			('code' in err && (err as { code: number }).code === 1))
 	);
 }
+
+export function roundToPrecision(coord: number, decimals = 2): number {
+	return Number(coord.toFixed(decimals));
+}
+
+export function roundTo1km(coord: number): number {
+	return roundToPrecision(coord, 2);
+}
