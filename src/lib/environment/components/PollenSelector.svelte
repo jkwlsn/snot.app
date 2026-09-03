3db1a96b73e92e76341d2b5c0c699ada52b7d187
Jake Wilson <hi@jkwlsn.dev>
2026-09-03 10:29:46 +0100

    Update `PollenSelector` component logic
    Persist `selectedPollenTypes` in local storage.

--- src/lib/environment/components/PollenSelector.svelte
+++ src/lib/environment/components/PollenSelector.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import { getEnvironmentState } from '$lib/environment';
 	import { getPollenName } from '$lib/environment/utils';
+	import { getSettingsContext } from '$lib/settings';
 	import type { PollenType } from '$lib/environment/types';
 
 	const env = getEnvironmentState();
+	const settings = getSettingsContext();
 
-	function togglePollen(pollenId: PollenType) {
+	function toggleSelectedPollenType(pollenId: PollenType) {
 		const index = env.selectedPollenTypes.indexOf(pollenId);
 		if (index > -1) {
 			env.selectedPollenTypes.splice(index, 1);
 		} else {
 			env.selectedPollenTypes.push(pollenId);
 		}
+		settings.update('selectedPollenTypes', [...env.selectedPollenTypes]);
 	}
 </script>
 
@@ -21,7 +24,7 @@
 			<input
 				type="checkbox"
 				checked={env.selectedPollenTypes.includes(pollenId)}
-				onchange={() => togglePollen(pollenId)}
+				onchange={() => toggleSelectedPollenType(pollenId)}
 			/>
 			{getPollenName(pollenId)}
 		</label>
