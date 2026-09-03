bc6b498a70ff6fbf70c67841e313901a7787e9bd
Jake Wilson <hi@jkwlsn.dev>
2026-09-03 09:01:53 +0100

    Remove redundant `SymptomConfig` type

--- src/lib/symptoms/types.ts
+++ src/lib/symptoms/types.ts
@@ -5,8 +5,3 @@ export type SymptomName = (typeof SYMPTOMS)[number]['name'];
 export type SymptomSeverity = (typeof SEVERITY_LEVELS)[number];
 
 export type SymptomFields = Record<SymptomName, SymptomSeverity>;
-
-export interface SymptomConfig {
-	name: string;
-	description: string;
-}
